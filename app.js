905dcb546be2a7f5eac8b201ecc7b04879722bef
Phat Duong <beebapcay@gmail.com>
2021-11-03 12:26:53 +0700

    Add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./src/routes/index');
 const classApiRouter = require('./src/api/classes');
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // DB connection
 require('./src/database/connection');
